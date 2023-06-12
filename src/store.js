8fa1c12f9707d559110e2ad4fb9a2c49c3392818
besufikad17 <besufikadtilahun1994@gmail.com>
2023-06-12 15:20:46 +0300

    zustand boilerplate added

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+const useStore = create(
+    persist(
+        (set) => {
+            return {
+                student: null,
+                registration_id: null,
+                token: null,
+
+                setStudent: (newStudent) => set((state) => ({ ...state, student: newStudent })),
+                setId: (newId) => set((state) => ({...state, registration_id: newId})),
+                setToken: (newToken) => set((state) => ({ ...state, token:newToken })),
+                logout: () => set((state) => ({ ...state,user : null, token: null }))
+            }
+        },
+        {
+            name: "user-info"
+        }
+    )
+)
+
+export { useStore }
