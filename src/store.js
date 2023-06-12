ee7b4236cfc3e0d94335cb6493c2d1153e43d016
besufikad17 <besufikadtilahun1994@gmail.com>
2023-06-12 15:26:40 +0300

    zustand boilerplate added

--- src/store.js
+++ src/store.js
@@ -1,2 +1,22 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+const useUser = create(
+    persist(
+        (set) => {
+            return {
+                user: null,
+                token: null,
+
+                setUser: (newUser) => set((state) => ({ ...state, user: newUser })),
+                setToken: (newToken) => set((state) => ({ ...state, token:newToken })),
+                logout: () => set((state) => ({ ...state,user : null, token: null }))
+            }
+        },
+        {
+            name: "user-info"
+        }
+    )
+)
 
 export { useUser }
